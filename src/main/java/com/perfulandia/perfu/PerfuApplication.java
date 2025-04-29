c9cbd04de0c7f1eb5c8b512cedf1236a986655f3
FMaulen <fabi.maulen@duocuc.cl>
2025-04-29 10:33:49 -0400

    First

---
+++ src/main/java/com/perfulandia/perfu/PerfuApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/perfu/PerfuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
