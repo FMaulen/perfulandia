6b944c6fc99389d57011d063af80d802ed821057
FMaulen <fabi.maulen@duocuc.cl>
2025-05-22 20:52:09 -0400

    Profe si lee esto porfavor tenga piedad

--- src/main/java/com/perfulandia/perfu/Services/InventarioService.java
+++ src/main/java/com/perfulandia/perfu/Services/InventarioService.java
@@ -23,6 +23,8 @@ public class InventarioService {
         for (Inventario inventario : inventarioRepository.findAll()){
             output += "ID Producto: " + inventario.getId() + "\n";
             output += "Stock Producto: " + inventario.getStock() + "\n";
+            output += "Nombre Producto: " + inventario.getProducto() + "\n";
+            output += "Nombre Sucursal: " + inventario.getSucursal() + "\n";
         }
 
         if (output.isEmpty()){
@@ -31,5 +33,60 @@ public class InventarioService {
             return output;
         }
     }
+
+    // Buscar por ID
+    public String buscarItemPorID(int id){
+        String output = "";
+        if (inventarioRepository.existsById(id)){
+            Inventario item = inventarioRepository.findById(id).get();
+            output += "ID Producto: " + item.getId() + "\n";
+            output += "Stock Producto: " + item.getStock() + "\n";
+            output += "Nombre Producto: " + item.getProducto() + "\n";
+            output += "Nombre Sucursal: " + item.getSucursal() + "\n";
+        } else {
+            return "No existe un item con ese ID";
+        }
+        return output;
+    }
+
+    // Borrar por ID
+    public String eliminarItemPorID(int id){
+        if (inventarioRepository.existsById(id)){
+            inventarioRepository.deleteById(id);
+            return "Item eliminado correctamente";
+        } else {
+            return "No existe un item con ese ID";
+        }
+    }
+
+    // Actualizar producto
+    public String actualizarItem(int id, Inventario item){
+        if (inventarioRepository.existsById(id)){
+            Inventario buscado = inventarioRepository.findById(id).get();
+            buscado.setStock(item.getStock());
+            buscado.setProducto(item.getProducto());
+            buscado.setSucursal(item.getSucursal());
+            inventarioRepository.save(buscado);
+            return "Item actualizado correctamente";
+        } else {
+            return "No existe un item con ese ID";
+        }
+
+    }
+
+    // Actualizar stock
+    public String actualizarStock(int id, int stock){
+        if (inventarioRepository.existsById(id)){
+            Inventario buscado = inventarioRepository.findById(id).get();
+            buscado.setStock(stock);
+            return "Stock actualizado correctamente";
+        } else {
+            return "No existe un item con ese ID";
+        }
+    }
+
+
+
+
 
 }
